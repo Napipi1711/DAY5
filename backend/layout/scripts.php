6b5f0a34b54804c4abd622fe53b8b04d53303d04
Napipi1711 <lhung89030@gmail.com>
2025-07-22 18:12:33 +0700

    Initial commit

---
+++ backend/layout/scripts.php
@@ -0,0 +1,4 @@
+<script src="/demo1/assets/vendor/jquery/jquery-3.7.1.min.js"></script>
+
+<script src="/demo1/assets/vendor/bootstrap/js/bootsctrap.min.js"></script>
+
---
+++ backend/layout/style.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="/demo1/assets/vendor/bootstrap/css/bootsctrap.min.css">
+<link rel="stylesheet" href="/demo1/assets/vendor/bootstrap/css/font-awesome.min.css">
